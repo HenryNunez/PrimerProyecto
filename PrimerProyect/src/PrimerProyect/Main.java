18ccc25de5ca5c6807bc18d831d15421a5bd22c7
HN <hn@desktop-qgh8rst>
2019-10-17 23:54:14 -0500

    Modificado por Nicolas Inchiglema

--- PrimerProyect/src/PrimerProyect/Main.java
+++ PrimerProyect/src/PrimerProyect/Main.java
@@ -2,6 +2,10 @@ package PrimerProyect;
 
 public class Main {
 
+	//Creado por Henry Nuñez
+
+	//Modificado por Nicolas Inchiglema
+
 	public static void main(String[] args) {
 		
 		System.out.println("Software II");
