b8e732ba41553302da153d4b0c4f2a7f73d1c8c4
GabyBayola <56320489+Bayola@users.noreply.github.com>
2019-10-20 20:43:20 -0500

    Update Main.java

--- PrimerProyect/src/PrimerProyect/Main.java
+++ PrimerProyect/src/PrimerProyect/Main.java
@@ -5,7 +5,29 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Software II");
+		
 
 	}
 
+	public void start(Stage primaryStage) {
+		int answer = MessageBox.show(primaryStage,
+						"Ejemplo de un DialogBox.\n\nLas opciones son las siguientes.\n[MessageBox.ICON_INFORMATION | MessageBox.OK | MessageBox.CANCEL]",
+						"Information dialog", 
+						MessageBox.ICON_INFORMATION| MessageBox.OK | MessageBox.CANCEL);
+
+		if (answer == MessageBox.OK) {
+			System.out.println("Ok");
+		} else if (answer == MessageBox.CANCEL) {
+			System.out.println("Cancel");
+		}
+
+		answer = MessageBox.show(primaryStage,
+						"Ejemplo de un mensaje de error.\n\nLas opciones son las siguientes.\n[MessageBox.ICON_ERROR]",
+						"Error dialog", 
+						MessageBox.ICON_ERROR);
+		System.out.println(answer);
+
+	}
+
+	
 }
